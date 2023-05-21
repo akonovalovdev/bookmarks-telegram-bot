e4a57727a3f7833d92dc0411e8dc1f39e4d93223
Александр <akonovalov.dev@gmail.com>
2023-05-21 11:42:07 +0300

    Updates the homepage

--- www/main.go
+++ www/main.go
@@ -4,9 +4,22 @@ import (
 	"fmt"
 	"net/http"
 )
+type User struct {
+	name string
+	age uint16
+	money int16
+	avgGrades, happiness float64
+}
+
+func (u User) getAllInfo() string {
+	return fmt.Sprintf("User name is: %s. He is %d and he has money equal: %d",
+	u.name, u.age, u.money)
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Home page")
+	bob := User{"Bob", 25, -50, 4.2, 0.8}
+	//bob := User{name: "Bob", age: 25, money: -50, avgGrades: 4.2, happiness: 0.8}
+	fmt.Fprintf(w, bob.getAllInfo())
 }
 
 func contactsPage(w http.ResponseWriter, r *http.Request) {
@@ -21,6 +34,9 @@ func handleRequest() {
 }
 
 func main() {
+	// var bob User =
+	// bob := User{name: "Bob", age: 25, money: -50, avgGrades: 4.2, happiness: 0.8}  User("Bob", 25, -50, 4.2, 0.8)
+
 	handleRequest()
 
 }
