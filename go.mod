18b9494ae4e961093b55c526cba9cae15ce5cf47
Александр <akonovalov.dev@gmail.com>
2023-05-27 12:09:00 +0300

    Fixes bugs in a new custom development environment

--- go.mod
+++ go.mod
@@ -2,3 +2,4 @@ module github.com/akonovalovdev/server
 
 go 1.20
 
+
--- main.go
+++ main.go
@@ -5,24 +5,23 @@ import (
 	"log"
 
 	tgClient "github.com/akonovalovdev/server/clients/telegram"
+	"github.com/akonovalovdev/server/consumer/event-consumer"
 	"github.com/akonovalovdev/server/events/telegram"
 	"github.com/akonovalovdev/server/storage/files"
-	"github.com/akonovalovdev/server/consumer/event-consumer"
 )
 
 const (
-	tgBotHost = "api.telegram.org"
+	tgBotHost   = "api.telegram.org"
 	storagePath = "files_storage"
-	batchSize = 100
+	batchSize   = 100
 )
 
-//токен 6143760943:AAEEJBrZPzkkSOh7ESj-RL6ms4ikFJF0cBI
-
 func main() {
-	//создаём телеграм клиент(клиент который общаяется с телеграмом): (тип Client и его методы реализованы в файле telegram.go в папке client)
+	//создаём телеграм клиент(клиент который общаяется с телеграмом):
+	//(тип Client и его методы реализованы в файле telegram.go в папке client)
 	// получает сообщения которые ему пишут и отправляет собственные
 	//tgClient = telegram.New(tgBotHost, mustToken())
-	
+
 	//создаём объект реализующий интерфейсы Processor и Fetcher
 	eventsProcessor := telegram.New(
 		tgClient.New(tgBotHost, mustToken()),
@@ -31,13 +30,13 @@ func main() {
 
 	log.Print("service started") //?????????????????????? для чего сообщения передаются через log (6й урокб 10я минута)
 
-	//запускаем консьюмер consumer.Strart(fetcher, processor)
+	//запускаем консьюмер consumer.Start(fetcher, processor)
 	consumer := event_consumer.New(eventsProcessor, eventsProcessor, batchSize)
 
 	if err := consumer.Start(); err != nil {
 		log.Fatal("service is stopped", err) // записываем в лог сообщение об ошибке и останавливаем программу
 	}
-}	
+}
 
 func mustToken() string {
 	token := flag.String(
